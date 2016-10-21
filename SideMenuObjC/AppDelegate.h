c7a53f180ccef67891822aa0dc9caefa4c6e50b7
Tom Thompson <tom@tsquaredapps.com>
2016-10-21 07:47:25 -0400

    Initial Commit

---
+++ SideMenuObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SideMenuObjC
+//
+//  Created by Thomas Thompson on 10/21/16.
+//  Copyright © 2016 Thomas Thompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SideMenuObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SideMenuObjC
+//
+//  Created by Thomas Thompson on 10/21/16.
+//  Copyright © 2016 Thomas Thompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
